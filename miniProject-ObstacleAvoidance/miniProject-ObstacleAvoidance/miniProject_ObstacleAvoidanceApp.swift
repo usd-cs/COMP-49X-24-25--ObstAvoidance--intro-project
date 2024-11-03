d044b5b81e93591e1265aeb6f62b0caff05c2caa
carlosbreach <cbreach@sandiego.edu>
2024-11-02 23:35:07 -0700

    Add initial Xcode project files

---
+++ miniProject-ObstacleAvoidance/miniProject-ObstacleAvoidance/miniProject_ObstacleAvoidanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  miniProject_ObstacleAvoidanceApp.swift
+//  miniProject-ObstacleAvoidance
+//
+//  Created by Carlos Breach on 11/2/24.
+//
+
+import SwiftUI
+
+@main
+struct miniProject_ObstacleAvoidanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ miniProject-ObstacleAvoidance/miniProject-ObstacleAvoidanceTests/miniProject_ObstacleAvoidanceTests.swift
@@ -0,0 +1,17 @@
+//
+//  miniProject_ObstacleAvoidanceTests.swift
+//  miniProject-ObstacleAvoidanceTests
+//
+//  Created by Carlos Breach on 11/2/24.
+//
+
+import Testing
+@testable import miniProject_ObstacleAvoidance
+
+struct miniProject_ObstacleAvoidanceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
