d95e64359674fe8435450f488e3e9ac1c8562a1a
daranda787 <daranda@sandiego.edu>
2024-11-03 14:01:44 -0800

    Saving first variation of Testing for Creating the User, Comment, and Post instances

---
+++ miniProject-ObstacleAvoidance/miniProject-ObstacleAvoidanceTests/DataTests.swift
@@ -0,0 +1,16 @@
+//
+//  DataTests.swift
+//  miniProject-ObstacleAvoidanceTests
+//
+//  Created by Darien Aranda on 11/3/24.
+//
+
+import Testing
+
+struct DataTests {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
