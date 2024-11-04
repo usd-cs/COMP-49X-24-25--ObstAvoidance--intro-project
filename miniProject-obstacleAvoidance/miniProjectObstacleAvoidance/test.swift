622d08c64629832c4467f1a3d76ae8f1637eadf5
carlosbreach <cbreach@sandiego.edu>
2024-11-03 22:32:15 -0800

    main and test files to give the group a basic undestanding of how different files are called on swift

---
+++ miniProject-obstacleAvoidance/miniProjectObstacleAvoidance/test.swift
@@ -0,0 +1,20 @@
+//
+//  test.swift
+//  miniProjectObstacleAvoidance
+//
+//  Created by Carlos Breach on 11/3/24.
+//
+
+func print_char(){
+    for x in "Venancio"{
+        print (x)
+    }
+}
+
+func test(){
+    print_char()
+    print("I think I finally get swift!")
+}
+
+
+
