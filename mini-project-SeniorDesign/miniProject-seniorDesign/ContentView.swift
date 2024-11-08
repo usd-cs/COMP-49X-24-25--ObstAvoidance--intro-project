b027eb152ee002e0e10d1116b980af94062bce88
daranda787 <daranda@sandiego.edu>
2024-11-07 21:26:35 -0800

    Hail Mary to fix the clone bug

--- mini-project-SeniorDesign/miniProject-seniorDesign/ContentView.swift
+++ mini-project-SeniorDesign/miniProject-seniorDesign/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("We're cooking with oil")
         }
         .padding()
     }
